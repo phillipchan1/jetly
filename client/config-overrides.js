d3237ab6b7aea0029446293df4f865737756fb28
phillipchan2 <phillip.chan@liferay.com>
2018-11-15 16:14:07 -0800

    Add sass to project

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,10 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireSass = require('react-app-rewire-scss');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
 	config = rewireReactHotLoader(config, env);
+	config = rewireSass(config, env);
+
 	return config;
 };
--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
 import { hot } from 'react-hot-loader';
+
+import Button from './components/atoms/Button/Button';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
 					<p>
-						Edit <code>src/App.js</code> and save to reload?
+						Edit <code>src/App.js</code> and save to reload!
 					</p>
 					<a
 						className="App-link"
@@ -20,6 +21,7 @@ class App extends Component {
 					>
 						Learn React
 					</a>
+					<Button />
 					<a href="http://localhost:5000/api/auth/google">
 						Login with Google
 					</a>
